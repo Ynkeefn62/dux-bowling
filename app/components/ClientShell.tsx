dbb3bcec17bfb33a1b7854cca5b69af9fdaa8d9d
Ynkeefn62 <andrew.boller@aol.com>
2025-12-28 03:21:25 -0500

    Create ClientShell.tsx

---
+++ app/components/ClientShell.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import HamburgerMenu from "./HamburgerMenu";
+import LoginButton from "./LoginButton";
+
+export default function ClientShell() {
+  return (
+    <>
+      <HamburgerMenu />
+      <LoginButton />
+    </>
+  );
+}
