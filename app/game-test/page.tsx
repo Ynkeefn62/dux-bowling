585cb3ea83228372c606ec6a8717684de4377b21
Ynkeefn62 <andrew.boller@aol.com>
2025-12-29 23:20:33 -0500

    Update page.tsx

--- app/game-test/page.tsx
+++ app/game-test/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import dynamic from "next/dynamic";
 
 const GameClient = dynamic(() => import("../components/GameClient"), { ssr: false });
