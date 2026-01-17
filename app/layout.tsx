970fb9f2120f934026fe386a3d8e29c2bc8a5d97
Ynkeefn62 <andrew.boller@aol.com>
2026-01-17 07:22:58 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-import TopBanner from "./components/TopBanner";
+import HamburgerMenu from "./components/HamburgerMenu";
+import LoginButton from "./components/LoginButton";
 import Footer from "./components/Footer";
 import CookieBanner from "./components/CookieBanner";
 
@@ -25,16 +26,44 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className} style={{ margin: 0 }}>
-        {/* Persistent top banner (menu + logo + login) */}
-        <TopBanner />
+        {/* Fixed top banner background */}
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 72,
+            background: "#000",
+            zIndex: 40,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          {/* Centered logo */}
+          <img
+            src="/dux-bowling-wordmark.png" // <-- update path if needed
+            alt="Dux Bowling"
+            style={{
+              height: 42,
+              width: "auto",
+              pointerEvents: "none",
+              userSelect: "none"
+            }}
+          />
+        </div>
 
-        {/* Push page content below fixed banner */}
+        {/* Existing fixed buttons */}
+        <HamburgerMenu />
+        <LoginButton />
+
+        {/* Page content offset below banner */}
         <div style={{ paddingTop: 72 }}>
           {children}
           <Footer />
         </div>
 
-        {/* Global cookie banner */}
         <CookieBanner />
       </body>
     </html>
