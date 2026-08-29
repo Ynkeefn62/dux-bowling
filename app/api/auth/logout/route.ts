4031d4c468deffb465b42c3e5f00754339e9d885
Ynkeefn62 <andrew.boller@aol.com>
2026-08-29 06:13:22 -0400

    Delete app directory

--- app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { clearAuthCookies } from "../_cookies";
-
-export async function POST() {
-  clearAuthCookies();
-  return NextResponse.json({ ok: true });
-}
